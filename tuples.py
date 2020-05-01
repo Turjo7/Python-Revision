74dbd1523128f207592b472ebbeb89629b1817d3
Turjo86 <pmj011151086@gmail.com>
2020-05-01 22:34:34 +0600

    Tuple Updated

---
+++ tuples.py
@@ -0,0 +1,15 @@
+numbers=(1,2,3,4,5)
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+# Difficult to modify
+
+coordinates = (1,2,3)
+x=coordinates[0]
+y=coordinates[1]
+z=coordinates[2]
+
+print(f'''{x} - {y} - {z}''')
+
+a,b,c = coordinates
+print(f'''{a} ->> {b} ->> {c}''')
